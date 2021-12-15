f9b484cba73b8e70b715e0ffd007495aa2bd8078
Jason Muffett <jason.muffett@splice.com>
2021-12-15 09:21:59 -0500

    initial commit

---
+++ parse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import re
+import pdb
+
+addr_file = "addresses.txt"
+
+file = open(addr_file)
+file_contents = file.read()
+addr_chunks = re.sub(r'\n\s*\n', '\n\n', file_contents).split("\n\n")
+
+for chunk in addr_chunks:
+    chunk_split = chunk.splitlines()
+    name = chunk_split[0]
+    addr = chunk_split[1:]
+    # addr_oneline = addr.replace('\n', ' ')
+    print(f"NAME: {name}")
+    print(f"ADDR: {addr}")
+    print('')
